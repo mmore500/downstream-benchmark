b43de5716394fe70ef7c48a6e9268a27bf8db43f
mmore500 <mmore500.login@gmail.com>
2025-01-19 16:21:00 +0000

    Deploying to binder from @ mmore500/downstream-benchmark@eba06bb9edefb18204b1cb06ed73f08da4f299f3 🚀

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -60,6 +60,10 @@ struct naive_steady_algo {
   static std::string_view get_algo_name() { return "naive_steady_algo"; }
 };
 
+struct zhao_steady_algo {
+  static std::string_view get_algo_name() { return "zhao_steady_algo"; }
+};
+
 struct control_ring_algo {
   static std::string_view get_algo_name() { return "control_ring_algo"; }
   static uint32_t _assign_storage_site(const uint32_t S, const uint32_t T) {
@@ -136,6 +140,51 @@ execute_naive_assign_storage_site(const uint32_t num_items) {
   return sizeof_vector(storage) + sizeof_vector(segment_lengths);
 }
 
+template <uint32_t num_sites>
+__attribute__((hot)) uint32_t
+execute_zhao_assign_storage_site(const uint32_t num_items) {
+  std::vector<uint32_t> segment_lengths;
+  std::vector<bool> storage;
+  segment_lengths.reserve(num_sites);
+  storage.reserve(num_sites);
+  DoNotOptimize(storage);
+
+  xorshift_generator gen{};
+  for (uint32_t i = 0; i < num_items; ++i) {
+    const bool data = gen() & 1;
+
+    if (storage.size() <= num_sites) {
+      storage.push_back(data);
+      segment_lengths.push_back(1);
+      continue;
+    }
+
+    if (segment_lengths[num_sites - 1] < segment_lengths[num_sites - 2]) {
+      segment_lengths[num_sites - 1] += 1;
+    } else {
+      storage.push_back(data);
+      segment_lengths.push_back(1);
+    }
+
+    const auto indexRange = std::views::iota(size_t{}, storage.size() - 1);
+    const auto collapse_idx = *std::ranges::min_element(
+        indexRange, [&](std::size_t i1, std::size_t i2) {
+          const auto sum1 = segment_lengths[i1] + segment_lengths[i1 + 1];
+          const auto sum2 = segment_lengths[i2] + segment_lengths[i2 + 1];
+          return sum1 < sum2;
+        });
+
+    segment_lengths[collapse_idx] += segment_lengths[collapse_idx + 1];
+    storage.erase(std::next(std::begin(storage), collapse_idx + 1));
+    segment_lengths.erase(
+        std::next(std::begin(segment_lengths), collapse_idx + 1));
+  }
+
+  DoNotOptimize(storage);
+  DoNotOptimize(gen.state);
+  return sizeof_vector(storage) + sizeof_vector(segment_lengths);
+}
+
 template <typename dstream_algo, uint32_t num_sites>
 __attribute__((hot)) uint32_t
 execute_dstream_assign_storage_site(const uint32_t num_items) {
@@ -165,6 +214,13 @@ template <uint32_t num_sites>
 struct execute_assign_storage_site<num_sites, naive_steady_algo> {
   static uint32_t operator()(const uint32_t num_items) {
     return execute_naive_assign_storage_site<num_sites>(num_items);
+  }
+};
+
+template <uint32_t num_sites>
+struct execute_assign_storage_site<num_sites, zhao_steady_algo> {
+  static uint32_t operator()(const uint32_t num_items) {
+    return execute_zhao_assign_storage_site<num_sites>(num_items);
   }
 };
 
@@ -226,6 +282,7 @@ int main() {
   benchmark_assign_storage_site<dstream_stretched_algo>(inserter);
   benchmark_assign_storage_site<dstream_tilted_algo>(inserter);
   benchmark_assign_storage_site<naive_steady_algo>(inserter);
+  benchmark_assign_storage_site<zhao_steady_algo>(inserter);
 
   std::cout << benchmark_result::make_csv_header();
   for (const auto &result : results)
