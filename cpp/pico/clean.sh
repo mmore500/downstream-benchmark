e2311239ee51040353e0081c6b10e4570133c2a6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-25 16:16:46 -0500

    Extend clean targets

--- cpp/pico/clean.sh
+++ cpp/pico/clean.sh
@@ -3,4 +3,4 @@ set -euo pipefail  # Exit on any error
 
 cd "$(dirname "$0")"
 
-rm -rf CMakeFiles pico CMakeCache.txt Makefile build
+rm -rf CMakeFiles pico CMakeCache.txt Makefile buildcMakeDoxyfile.in CMakeDoxygenDefaults.cmake compile_commands.json main.{bin,elf,elf.map,hex,uf2} pico_flash_region.ld
