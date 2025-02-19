9604f691a374245c1b8dbd1dc8665a97a0e00d4f
mmore500 <mmore500.login@gmail.com>
2025-02-19 17:17:33 +0000

    Deploying to binder-2024-10-20-example.ipynb from @ mmore500/downstream-benchmark@963429f45fb6869a98171fec277e2b9c680f9ba8 🚀

--- cpp/lint.sh
+++ cpp/lint.sh
@@ -5,7 +5,10 @@ cd "$(dirname "$0")"
 
 # C++ linting with clang-tidy
 echo "Running C++ linting..."
-find "native/main.cpp" "main_impl.hpp" | while read -r file; do
+find \
+    "native/main.cpp" "pico/main.cpp" \
+    "include/" -type f -name "*.hpp" \
+| while read -r file; do
     clang-tidy "$file" -- -std=c++23 -I.
 done
 
