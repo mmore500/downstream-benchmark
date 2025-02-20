a241f463e71a75b6be8b85cf830792f60cf33b61
mmore500 <mmore500.login@gmail.com>
2025-02-20 20:36:47 +0000

    Deploying to binder-2025-01-18-qos-dstream-vs-naive-steady.ipynb from @ mmore500/downstream-benchmark@e53cbea5dbce91bde021b9894171c8c517bbb929 🚀

--- cpp/include/algo/dstream_stretched_algo.hpp
+++ cpp/include/algo/dstream_stretched_algo.hpp
@@ -1,104 +1,13 @@
 #pragma once
 
-#include <algorithm>
-#include <bit>
 #include <cassert>
-#include <concepts>
-#include <optional>
 #include <string_view>
 
-#include "../../downstream/include/downstream/_auxlib/DOWNSTREAM_UINT.hpp"
-#include "../../downstream/include/downstream/_auxlib/overflow_shr.hpp"
-#include "../../downstream/include/downstream/_auxlib/std_bit_casted.hpp"
 #include "../../downstream/include/downstream/dstream/dstream.hpp"
 
-template <uint32_t S> struct bs_table {
-  constexpr bs_table() : data() {
-    constexpr uint32_t s = std::bit_width(S) - 1;
-    for (uint32_t blT = 0; blT < 32; ++blT) {
-      const uint32_t t = blT - std::min(s, blT); // Current epoch
-      const uint32_t blt = std::bit_width(t);    // Bit length of t
-
-      bool epsilon_tau = std::bit_floor(t << 1) > t + blt; // Correction factor
-      const uint32_t tau = blt - epsilon_tau;              // Current meta-epoch
-      data[blT] = std::max<uint32_t>(S >> (tau + 1), 1);
-      // ^^^ Num bunches available to h.v.
-    }
-  }
-  uint8_t data[32];
-};
-
-inline uint32_t ctz_naive(uint32_t x) {
-  if (x & 1)
-    return 0;
-  else if (x & 2)
-    return 1;
-  else if (x & 4)
-    return 2;
-  else if (x & 8)
-    return 3;
-  else if (x & 16)
-    return 4;
-
-  uint32_t n = 5;
-  x >>= 5;
-  while ((x & 1) == 0) {
-    x >>= 1;
-    ++n;
-  }
-  return n;
-}
-
-// https://graphics.stanford.edu/~seander/bithacks.html
-inline uint32_t log2_naive(uint32_t v) {
-  constexpr char LogTable256[256] = {
-#define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
-      static_cast<char>(-1),
-      0,
-      1,
-      1,
-      2,
-      2,
-      2,
-      2,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      LT(4),
-      LT(5),
-      LT(5),
-      LT(6),
-      LT(6),
-      LT(6),
-      LT(6),
-      LT(7),
-      LT(7),
-      LT(7),
-      LT(7),
-      LT(7),
-      LT(7),
-      LT(7),
-      LT(7)};
-
-  uint32_t r;  // r will be lg(v)
-  uint32_t tt; // temporaries
-
-  if ((tt = (v >> 24))) {
-    r = 24 + LogTable256[tt];
-  } else if ((tt = (v >> 16))) {
-    r = 16 + LogTable256[tt];
-  } else if ((tt = (v >> 8))) {
-    r = 8 + LogTable256[tt];
-  } else {
-    r = LogTable256[v];
-  }
-  return r;
-}
+#include "../aux/ctz_naive.hpp"
+#include "../aux/log2_naive.hpp"
+#include "./dstream_helpers.hpp"
 
 template <uint32_t S>
 uint32_t _dstream_stretched_assign_storage_site_impl(const uint32_t T) {
@@ -106,15 +15,14 @@ uint32_t _dstream_stretched_assign_storage_site_impl(const uint32_t T) {
   constexpr uint32_t _1{1};
   namespace aux = downstream::_auxlib;
 
-  const uint32_t blT = log2_naive(T);
+  const uint32_t blT = log2_naive(T) + bool(T);
   const uint32_t h = ctz_naive(T + _1); // Current hanoi value
 
   // DEPENDS ON t, h
   const uint32_t i = T >> (h + _1);
   // ^^^ Hanoi value incidence (i.e., num seen)
 
-  constexpr bs_table<S> bs{};
-  const uint32_t b = bs.data[blT]; // Num bunches available to hanoi value
+  const uint32_t b = lookup_bs<S>(blT); // Num bunches available to hanoi value
 
   // DEPENDS ON t, h
   if (i >= b) [[likely]] { // If seen more than sites reserved to hanoi value...
@@ -122,27 +30,8 @@ uint32_t _dstream_stretched_assign_storage_site_impl(const uint32_t T) {
   }
 
   const uint32_t b_l = i; // Logical bunch index...
-  // ... i.e., in order filled (increasing nestedness/decreasing init size r)
-
-  // Need to calculate physical bunch index...
-  // ... i.e., position among bunches left-to-right in buffer space
-  const uint32_t v =
-      std::bit_width(b_l); // Nestedness depth level of physical bunch
-  const uint32_t w =
-      (S >> v) * (v != 0); // Num bunches spaced between bunches in nest level
-  const uint32_t o =
-      w >> _1; // Offset of nestedness level in physical bunch order
-  const uint32_t p =
-      b_l - std::bit_floor(b_l);  // Bunch position within nestedness level
-  const uint32_t b_p = o + w * p; // Physical bunch index...
-  // ... i.e., in left-to-right sequential bunch order
-
-  // Need to calculate buffer position of b_p'th bunch
-  const bool epsilon_k_b = (b_l != 0); // Correction factor for zeroth bunch...
-  // ... i.e., bunch r=s at site k=0
-  const uint32_t k_b = (b_p << 1) +
-                       aux::popcount_casted<uint32_t>((S << 1) - b_p) - 1 -
-                       epsilon_k_b; // Site index of bunch
+  constexpr kb_table<S> kb{};
+  const auto k_b = kb.data[b_l];
 
   return k_b + h; // Calculate placement site...
                   // ... where h.v. h is offset within bunch
